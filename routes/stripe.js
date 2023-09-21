10279db7430a91c74641efb801ad899a979b6db5
Abhay <abhaypawar596@gmail.com>
2023-09-21 19:50:15 +0530

    first commit

---
+++ routes/stripe.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+const KEY = process.env.STRIPE_KEY
+const stripe = require("stripe")(KEY);
+
+router.post("/payment", (req, res) => {
+  stripe.paymentIntents.create({
+    amount: 500,
+    currency: 'gbp',
+    payment_method: 'pm_card_visa',
+  },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
+});
+
+module.exports = router;
